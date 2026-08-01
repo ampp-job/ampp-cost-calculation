ac833ab8a032175c9f8a793bfc82e3448a686087
ampp-job <azhuravlev.job@gmail.com>
2026-08-02 02:46:23 +0300

    Update cache version from ampp-v1 to ampp-v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'ampp-v1';
+const CACHE = 'ampp-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
